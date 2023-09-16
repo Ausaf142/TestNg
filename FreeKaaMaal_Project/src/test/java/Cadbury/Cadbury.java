389c8d49ef4379945243e9d9d9411f4475fddb5a
ausaf <ausaf142@gmail.com>
2023-09-16 16:09:13 +0530

    java prog

--- FreeKaaMaal_Project/src/test/java/Cadbury/Cadbury.java
+++ FreeKaaMaal_Project/src/test/java/Cadbury/Cadbury.java
@@ -27,43 +27,40 @@ public class Cadbury {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://cadburygifting.in/track-my-order/");
 		
-			for(int row=0;row<12;row++) {
+			for(int row=0;row<4;row++) {
 				System.out.println(row);
 				int cell=0;
 				ExcelUtility excel =new ExcelUtility();
 				String id =excel.getNumericCellValue(excelPtah,page,row,cell);
-//			System.out.println(id);
+//			    System.out.println(id);
 				
 				int cell2=cell+1;
 				String email = excel.getStringCellValue(excelPtah,page,row,cell2);
-//			System.out.println(email);
+//			    System.out.println(email);
 
 
 				driver.findElement(By.id("trackOderId")).sendKeys("00"+id);
 				driver.findElement(By.id("email")).sendKeys(email);
-//				driver.findElement(By.xpath("//button[@aria-label=\"Close Banner\"]/ancestor::div[@class='ot-sdk-row']/descendant::button[contains(@class,'onetrust-close-btn-handler onetrust-close-btn-ui banner-close-button onetrust-lg close-icon')]")).click();
+//
 				Thread.sleep(1000);
 				driver.findElement(By.xpath("//button[text()='Track Order']")).click();
 				Thread.sleep(4000);
-//				TakesScreenshot ts= (TakesScreenshot)driver;
-//				File SS =ts.getScreenshotAs(OutputType.FILE);
-//				File f =new File("./New folder/"+id+".png");
-//				Files.copy(SS, f);
+
 				int cell3=cell2+1;
-				String status = driver.findElement(By.xpath("//div[@class='right']/../.")).getText();
-//				ExcelUtility.writeValue(id, page, row, cell2, cell3, status);
+				String status = driver.findElement(By.xpath("(//div[@class='right']/.)[1]")).getText();
+//				String status = driver.findElement(By.xpath("//div[@class='right']/../.")).getText();
+
 				excel.writeValue(excelPtah, page, row, cell2, cell3, status);
+				String amount = driver.findElement(By.xpath("//span[@class='total-amount']")).getText();
+				int cell4=cell2+2;
+				excel.writeValue(excelPtah, page, row, cell2, cell4, amount);
 //				String status = driver.findElement(By.xpath("//div[@class='right']/ancestor::div[@class='order-details-out']")).getText();
 				
 				System.out.println(status);
 				driver.navigate().refresh();
 			
 			}
-			System.out.println("EXCEL SHEET AUTO RUN COMPLETED");
-			driver.quit();
-		
-		
-		
-		
+	   System.out.println("EXCEL SHEET AUTO RUN COMPLETED");
+	   driver.quit();
 }
 }
